ab895a1d0c2e4dc43a286bc50c8e1acd8d0f4826
Ivan Nazimov <ivan.nazimov@gmail.com>
2022-07-06 18:17:53 +0300

    Massive rewrite of session handler, added separate dispatcher and supervision

---
+++ ktor-kafka/ktor-kafka-fortunes-receiver/src/main/kotlin/com/lonerx/FortuneSessionHandler.kt
@@ -0,0 +1,126 @@
+package com.lonerx
+
+import com.lonerx.model.Fortune
+import io.ktor.websocket.CloseReason
+import io.ktor.websocket.Frame
+import io.ktor.websocket.WebSocketSession
+import io.ktor.websocket.close
+import io.ktor.websocket.readText
+import kotlinx.coroutines.CompletableJob
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import mu.KLogging
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import java.time.Duration
+import java.util.Properties
+import java.util.UUID
+
+class FortuneSessionHandler(
+    private val session: WebSocketSession
+) : CoroutineScope, KLogging() {
+
+    private val supervisor = SupervisorJob()
+    override val coroutineContext = Dispatchers.IO + supervisor + CoroutineExceptionHandler { _, t ->
+        logger.error(t) { "An error occurred during coroutine execution, stopping everything" }
+        supervisor.cancel()
+    }
+
+    private val incoming = session.incoming
+    private val outgoing = session.outgoing
+
+    @Suppress("MemberVisibilityCanBePrivate")
+    fun start(): CompletableJob {
+        launch {
+            echoLoop()
+        }
+        launch {
+            kafkaLoop()
+        }
+        return supervisor
+    }
+
+    suspend fun startBlocking() = start().join()
+
+    private suspend fun kafkaLoop() {
+        val consumer = createKafkaConsumer()
+        consumer.subscribe(listOf(TOPIC))
+        try {
+            while (isActive) {
+                consumer.poll(Duration.ofMillis(POLL_DURATION_MILLIS)).forEach { record ->
+                    val fortune = Json.decodeFromString<Fortune>(record.value())
+                    logger.debug { "kafkaLoop: received fortune #${fortune.id}" }
+                    outgoing.send(Frame.Text("FORTUNE #${fortune.id}:\n${fortune.fortune}\n"))
+                }
+            }
+        } finally {
+            logger.debug { "kafkaLoop: cleaning" }
+            consumer.unsubscribe()
+            consumer.close()
+            logger.debug { "kafkaLoop: stopping supervisor" }
+            supervisor.cancel()
+        }
+    }
+
+    private suspend fun echoLoop() {
+        try {
+            for (frame in incoming) {
+                parseIncomingFrame(frame)
+            }
+        } finally {
+            logger.debug { "echoLoop: stopping supervisor" }
+            supervisor.cancel()
+        }
+    }
+
+    private suspend fun parseIncomingFrame(frame: Frame) {
+        when (frame) {
+            // If frame is text, we just echo it back to client
+            // If text is 'bye', we close websockets session
+            is Frame.Text -> {
+                val text = frame.readText()
+                logger.debug("Text frame received: $text, echo back to client")
+                outgoing.send(Frame.Text("YOU WROTE: $text"))
+                if (text.equals("bye", ignoreCase = true)) {
+                    session.close(CloseReason(CloseReason.Codes.NORMAL, "Good bye!"))
+                }
+            }
+            // For other types of frames we just log the fact of their appearance
+            is Frame.Binary -> {
+                logger.debug("Binary frame received")
+            }
+            is Frame.Close -> {
+                logger.debug("Close frame received")
+            }
+            is Frame.Ping -> {
+                logger.debug("Ping frame received")
+            }
+            is Frame.Pong -> {
+                logger.debug("Pong frame received")
+            }
+        }
+    }
+
+    private fun createKafkaConsumer() = KafkaConsumer<String, String>(
+        Properties().apply {
+            put("group.id", UUID.randomUUID().toString())
+            put("bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS)
+            put("key.deserializer", KAFKA_KEY_DESERIALIZER)
+            put("value.deserializer", KAFKA_VALUE_DESERIALIZER)
+        }
+    )
+
+    companion object {
+        const val TOPIC = "lonerx.dev.fortunes"
+        const val POLL_DURATION_MILLIS = 200L
+
+        const val KAFKA_BOOTSTRAP_SERVERS = "kafka.local:9092"
+        const val KAFKA_KEY_DESERIALIZER = "org.apache.kafka.common.serialization.StringDeserializer"
+        const val KAFKA_VALUE_DESERIALIZER = "org.apache.kafka.common.serialization.StringDeserializer"
+    }
+}
--- ktor-kafka/ktor-kafka-fortunes-receiver/src/main/kotlin/com/lonerx/ktor/configureWebSockets.kt
+++ ktor-kafka/ktor-kafka-fortunes-receiver/src/main/kotlin/com/lonerx/ktor/configureWebSockets.kt
@@ -1,11 +1,6 @@
 package com.lonerx.ktor
 
-import com.lonerx.ktor.KafkaConfig.KAFKA_BOOTSTRAP_SERVERS
-import com.lonerx.ktor.KafkaConfig.KAFKA_KEY_DESERIALIZER
-import com.lonerx.ktor.KafkaConfig.KAFKA_VALUE_DESERIALIZER
-import com.lonerx.ktor.KafkaConfig.POLL_DURATION_MILLIS
-import com.lonerx.ktor.KafkaConfig.TOPIC
-import com.lonerx.model.Fortune
+import com.lonerx.FortuneSessionHandler
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
 import io.ktor.server.application.log
@@ -14,20 +9,8 @@ import io.ktor.server.websocket.WebSockets
 import io.ktor.server.websocket.pingPeriod
 import io.ktor.server.websocket.timeout
 import io.ktor.server.websocket.webSocket
-import io.ktor.websocket.CloseReason
-import io.ktor.websocket.Frame
-import io.ktor.websocket.close
-import io.ktor.websocket.readText
 import io.ktor.websocket.send
-import kotlinx.coroutines.cancelAndJoin
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.launch
-import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
-import org.apache.kafka.clients.consumer.KafkaConsumer
 import java.time.Duration
-import java.util.Properties
-import java.util.UUID
 
 @Suppress("LongMethod")
 fun Application.configureWebSockets() {
@@ -44,75 +27,14 @@ fun Application.configureWebSockets() {
             send("Welcome to fortunes world!")
             send("Any text you enter will be echoed, enter 'bye' to quit")
 
-            val fortuneReaderLoop = launch {
-                application.log.debug("starting kafka reader loop")
-                val consumer = createFortunesKafkaReceiver()
-                consumer.subscribe(listOf(TOPIC))
-                try {
-                    while (isActive) {
-                        consumer.poll(Duration.ofMillis(POLL_DURATION_MILLIS)).forEach {
-                            val fortune = Json.decodeFromString<Fortune>(it.value())
-                            application.log.debug("received fortune #${fortune.id}")
-                            send(fortune.fortune)
-                        }
-                    }
-                } finally {
-                    application.log.debug("kafka reader loop done, cleaning resources")
-                    consumer.apply {
-                        unsubscribe()
-                        close()
-                    }
-                }
-            }
+//            val sessionJob = FortuneSessionHandler(this).start()
+//            application.log.debug("session handler started")
+//            sessionJob.join()
+//            application.log.debug("session handler finished")
 
-            try {
-                for (frame in incoming) {
-                    when (frame) {
-                        is Frame.Text -> {
-                            val text = frame.readText()
-                            application.log.debug("Text frame received: $text")
-                            send("echo: $text")
-                            if (text.equals("bye", ignoreCase = true)) {
-                                close(CloseReason(CloseReason.Codes.NORMAL, "Good bye!"))
-                            }
-                        }
-                        is Frame.Binary -> {
-                            application.log.debug("Binary frame received")
-                        }
-                        is Frame.Close -> {
-                            application.log.debug("Close frame received")
-                        }
-                        is Frame.Ping -> {
-                            application.log.debug("Ping frame received")
-                        }
-                        is Frame.Pong -> {
-                            application.log.debug("Pong frame received")
-                        }
-                    }
-                }
-            } finally {
-                application.log.debug("finally block: shutting down reader loop")
-                fortuneReaderLoop.cancelAndJoin()
-                application.log.debug("finally block: reader loop coroutine joined")
-            }
+            application.log.debug("starting session handler")
+            FortuneSessionHandler(this).startBlocking()
+            application.log.debug("session handler finished")
         }
     }
-}
-
-fun createFortunesKafkaReceiver() = KafkaConsumer<String, String>(
-    Properties().apply {
-        put("group.id", UUID.randomUUID().toString())
-        put("bootstrap.servers", KAFKA_BOOTSTRAP_SERVERS)
-        put("key.deserializer", KAFKA_KEY_DESERIALIZER)
-        put("value.deserializer", KAFKA_VALUE_DESERIALIZER)
-    }
-)
-
-object KafkaConfig {
-    const val TOPIC = "lonerx.dev.fortunes"
-    const val POLL_DURATION_MILLIS = 200L
-
-    const val KAFKA_BOOTSTRAP_SERVERS = "kafka.local:9092"
-    const val KAFKA_KEY_DESERIALIZER = "org.apache.kafka.common.serialization.StringDeserializer"
-    const val KAFKA_VALUE_DESERIALIZER = "org.apache.kafka.common.serialization.StringDeserializer"
 }
